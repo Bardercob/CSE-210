ddcddbb06c354ca152549423423387eca51b9169
Bardercob <ctrskiyler@gmail.com>
2025-02-26 09:46:07 -0700

    added some comments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,3 +1,11 @@
+/*
+Program name: Prep1
+CSE 210
+Author: Skyler Tietjen
+
+Purpose: This program will ask the user for their first and last name, then print
+it to the console
+*/
 using System;
 
 class Program
@@ -5,9 +13,9 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string lastname = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
