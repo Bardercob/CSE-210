28017fb0a70ed27e4744f8b215c89d620588dbfe
Bardercob <ctrskiyler@gmail.com>
2025-02-27 09:59:19 -0700

    initial changes for prep2. did some practice in sandbox

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+        string letterGrade = "N";
+        int gradeRemainder = gradePercentage % 10;
+        string letterSign = "N";
+        string aOrAn = "";
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (gradeRemainder >= 7 && (letterGrade != "A" && letterGrade != "F"))
+        {
+            letterSign = "+";
+        }
+        else if (gradeRemainder <= 3 && letterGrade != "F")
+        {
+            letterSign = "-";
+        }
+        else
+        {
+            letterSign = "";
+        }
+        if (letterGrade == "A" || letterGrade == "F")
+        {
+            aOrAn = "n";
+        }
+
+        Console.WriteLine($"You got a{aOrAn} {letterGrade}{letterSign}");
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+
+        {
+            Console.WriteLine("You did not pass, you will need to take it again");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,13 +1,58 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
-{
+{   
+    public static int Addnumbers(int n, int m)
+    {
+        return n + m;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sand World!");
-        Console.WriteLine("Hello class");
-        Console.WriteLine("please enter your name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine($"Your name is {name}");
+        // for(float i = 0; i < 20; i += 0.3F)
+        // {
+        //     Console.WriteLine($"{i + 1}: bob");
+        // }
+
+        List<int> numbers = new List<int>{1, 2, 3, 4, 5};
+        numbers.Add(10);
+
+        foreach(int x in numbers)
+        {
+            Console.WriteLine($"{x}");
+        }
+
+        // bool correctInput = false;
+        // while(!correctInput)
+        // {
+        //     Console.Write("please input your age ");
+        //     int age = int.Parse(Console.ReadLine());
+        //     if (age >= 0 && age <= 120)
+        //     {
+        //         Console.WriteLine($"Your age is: {age}");
+        //         correctInput = true;
+        //     }
+        // }
+        // bool correctInput = false;
+        // do
+        // {
+        //     Console.Write("please input your age ");
+        //     int age = int.Parse(Console.ReadLine());
+        //     if (age >= 0 && age <= 120)
+        //     {
+        //         Console.WriteLine($"Your age is: {age}");
+        //         correctInput = true;
+        //     }
+        // } while(!correctInput);
+
+        Random newRandomNumber = new Random();
+        for(int x = 0; x < 10; x++)
+        {
+            int number = newRandomNumber.Next(1, 1000);
+            Console.WriteLine($"{number}");
+        }
+
+        int total = Addnumbers(4, 5);
+        Console.WriteLine($"{total}");
     }
 }
