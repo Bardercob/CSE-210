588bb203869d53781d44c3f0f71e533bb993652f
Bardercob <ctrskiyler@gmail.com>
2025-03-03 09:07:20 -0700

    completed prep3 assighnment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(0, 100) + 1;
+        int numberGuess = -19084237;
+        Console.WriteLine("Welcome to \"Guess The Number!\"");
+        
+        while (magicNumber != numberGuess)
+        {
+            Console.Write("What is your guess? ");
+            numberGuess = int.Parse(Console.ReadLine());
+            if (magicNumber > numberGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < numberGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
